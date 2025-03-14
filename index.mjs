683c1b54255aef15018a9cfa08aab6f9ac192bdc
Umer Saleem <162819866+UmerSaleemUltra@users.noreply.github.com>
2025-03-14 05:30:12 +0500

    first commit

---
+++ index.mjs
@@ -0,0 +1,33 @@
+import express from 'express';
+import ytdl from 'ytdl-core';
+
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
+app.get('/api/download/youtube', async (req, res) => {
+  const { url } = req.query;
+
+  if (!url || !ytdl.validateURL(url)) {
+    return res.status(400).send('Invalid YouTube URL');
+  }
+
+  try {
+    const info = await ytdl.getInfo(url);
+    const format = ytdl.chooseFormat(info.formats, { quality: '18' }) || ytdl.chooseFormat(info.formats, { quality: 'highest' });
+
+    res.setHeader('Content-Disposition', `attachment; filename="video.mp4"`);
+    res.setHeader('Content-Type', 'video/mp4');
+
+    ytdl(url, { format }).pipe(res);
+  } catch (error) {
+    console.error('Download error:', error.message);
+    res.status(500).send('Error downloading YouTube video');
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
